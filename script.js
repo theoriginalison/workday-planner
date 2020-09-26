ae3dd47efb0edca1f0b3df248da5e2a2ee4a6897
Alison Lubar <aslubar@gmail.com>
2020-09-25 21:39:31 -0400

    added JS file, created variables, and CSS selector add/remove

---
+++ script.js
@@ -0,0 +1,30 @@
+//need help w responsiveness?
+
+var saveBtn = $("#saveBtn");
+var textarea = $("textarea");
+var description = $(".description");
+
+var NOW = moment().format();
+
+console.log(NOW);
+
+//if the current time is within the same hour, else
+if (NOW === 21:35:52-04:00) {
+    console.log("it's that v specific time")
+    
+}
+else {
+    console.log("it's not that time rn")
+}
+
+//to make past time
+$(".description").removeClass("present future");
+
+//to make present time
+$(".description").removeClass("past future");
+
+//to make future time
+$(".description").removeClass("present past");
+
+//need an event handler, so using this onclick event
+$(saveBtn).click(function () {});
