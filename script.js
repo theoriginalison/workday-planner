dff19e8195502aa80dcc1d16fc391f1d934f9c1e
Alison Lubar <aslubar@gmail.com>
2020-09-29 18:31:14 -0400

    added comments in JS to write the README

--- script.js
+++ script.js
@@ -3,13 +3,12 @@
 //NOW YOU ONLY NEED THE HOUR! :D
 var NOW = parseInt(moment().format("HH"));
 
+// `${the variable goes here, like moment().format(.....)}, ${secondvariable which would be }`
 var today = moment().format("dddd") + ", " + moment().format("MMMM Do");
 
 //will parse whatever's passed, and if there's nothing, then it will be blank, hence the empty {}
+//if there's nothing there, it will be saved as an empty object
 var task = JSON.parse(localStorage.getItem("task")) || {};
-
-console.log(today);
-console.log(NOW);
 
 $("#currentDay").append(today);
 
@@ -18,6 +17,8 @@ var times = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 for (let index = 0; index < times.length; index++) {
   if (NOW > parseInt(times[index])) {
     //template literals-- look this up-- backticks indicate that it's a template literal, and then use the ${} to indicate that this will be JS
+    //template/STRING literal -- a way to write a string and plug variables into it, and have them automatically fill rather than string concatenation (like line 6); this concatenates all of the times like id=10, etc., together so you don't have to write all of them out
+    //"#"+times[index]
     $(`#${times[index]}`).removeClass("present future");
   } else if (NOW < parseInt(times[index])) {
     $(".description").removeClass("present past");
@@ -35,6 +36,7 @@ $(".saveBtn").click(function () {
   //attr gets or sets value
   var timeID = $(this).prev().attr("id");
   //[] references key in an object; adds the key to the object and matches up with the value SO when we get something from local storage, we can update the text area and value dynamically/responsively
+  //inserting the userInput into the task array that also has the timestamp/timeID (the key inside the object)
   task[timeID] = userInput;
   //1st parameter is the key, then the value of what we're setting it to-- which is the variable!!
   localStorage.setItem("task", JSON.stringify(task));
@@ -46,6 +48,8 @@ $(".saveBtn").click(function () {
 //uses template literals
 //"property" is like the iterater in the "for-in" loop, like "index" is the iterater in the regular "for" loop
 //maybe put this in a function and call it?
+//this means that it stays when the page is refreshed-- goes through the "task" object to see if it exists, and if it does, it keeps it in the page (or puts it back into the page) when the page loads
+//a fancy for loop that goes through everything in the task array, like task[i] and pulls out every property name (the IDs)
 for (const property in task) {
   $(`#${property}`).children("textarea").val(task[property]);
 }
