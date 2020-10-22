60368f3c2e1759fe5136ae32dda62c1e6f11cb67
Alison Lubar <aslubar@gmail.com>
2020-10-21 20:11:06 -0400

    removed test hours and added comments to JS

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 var NOW = parseInt(moment().format("HH"));
-console.log(NOW)
-//remove before final!!
-NOW -= 5
-console.log(NOW)
+
 var today = moment().format("dddd") + ", " + moment().format("MMMM Do");
 
 var task = JSON.parse(localStorage.getItem("task")) || {};
@@ -10,6 +7,8 @@ var task = JSON.parse(localStorage.getItem("task")) || {};
 var times = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 
 $("#currentDay").append(today);
+
+//checks the current time to determine CSS class of past, present, or future
 
 for (let index = 0; index < times.length; index++) {
   if (NOW > parseInt(times[index])) {
@@ -20,6 +19,8 @@ for (let index = 0; index < times.length; index++) {
     $(`#${times[index]}`).removeClass("past future");
   }
 }
+
+//save task functionality
 
 $(".saveBtn").click(function () {
   var userInput = $(this).prev().children("textarea").val();
